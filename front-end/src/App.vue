c268382a264a9c9c0ca93e6293b8003f3a04f3c3
Shawna <85918539+snnn333@users.noreply.github.com>
2022-02-14 20:16:03 -0800

    Add files via upload

---
+++ front-end/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    width:100%;
+    height: 100%;
+    background: url("assets/pic/bg/4.png");
+
+  }
+  html,body,#app{
+    width:100%;
+    height: 100%;
+    background-color: rgba(246,246,246,1);
+    margin:0;
+    padding:0;
+  }
+</style>
